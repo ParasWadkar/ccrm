c3ae97123f1fb7e827ab6a3a489e5dfb867fc0cd
ParasWadkar <67570820+ParasWadkar@users.noreply.github.com>
2025-09-25 22:20:44 +0530

    Delete ccrm directory

--- ccrm/exception/DuplicateEnrollmentException.java
+++
@@ -1,7 +0,0 @@
-package edu.ccrm.exception;
-
-public class DuplicateEnrollmentException extends RuntimeException {
-    public DuplicateEnrollmentException(String message) {
-        super(message);
-    }
-}
